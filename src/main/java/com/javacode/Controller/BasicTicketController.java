4e960b31ec02345873fe121e0097381f0ce31b39
Muhammad Bakr <mbakr7757@gmail.com>
2021-07-21 19:58:13 +0200

    Update BasicTicketController

--- src/main/java/com/javacode/Controller/BasicTicketController.java
+++ src/main/java/com/javacode/Controller/BasicTicketController.java
@@ -4,15 +4,11 @@ package com.javacode.Controller;
 import com.javacode.Interface.IBasicTicketService;
 import com.javacode.Model.BasicTicket;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class BasicTicketController
